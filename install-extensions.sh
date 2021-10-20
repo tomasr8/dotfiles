998d4672e861c2fdce6c3e0a65f3cb4881fc0b30
Tomas Roun <rountoma@fel.cvut.cz>
2021-10-20 10:47:57 +0200

    add ssh tunnel

---
+++ install-extensions.sh
@@ -0,0 +1,8 @@
+code --install-extension akamud.vscode-theme-onedark
+code --install-extension be5invis.vscode-custom-css
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-vscode-remote.remote-ssh
+code --install-extension ms-vscode-remote.remote-ssh-edit
---
+++ tunnel.sh
@@ -0,0 +1,2 @@
+ssh -D 9000 -C -N -v USER@HOST.cern.ch
+#ssh -D 9000 -C -N -v -v -v --proxy-type socks4 USER@HOST.cern.ch
